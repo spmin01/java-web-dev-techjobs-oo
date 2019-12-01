c326a5e4dd58fb190a64bd7b52abd7a19c087e64
Shaun Minnihan <spminnihan@gmail.com>
2019-11-30 23:27:14 -0600

    refactored w/ JobField class, combining functionality with Location, Employer, PositionType, and CoreCompetency

--- src/org/launchcode/techjobs_oo/CoreCompetency.java
+++ src/org/launchcode/techjobs_oo/CoreCompetency.java
@@ -2,26 +2,10 @@ package org.launchcode.techjobs_oo;
 
 import java.util.Objects;
 
-public class CoreCompetency {
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
-    public CoreCompetency() {
-        this.id = nextId;
-        nextId++;
-    }
+public class CoreCompetency extends JobField {
 
     public CoreCompetency(String value) {
-        this();
-        this.value = value;
-    }
-
-    // Custom toString, equals, and hashCode methods:
-
-    @Override
-    public String toString() {
-        return value;
+        super(value);
     }
 
     @Override
@@ -29,26 +13,6 @@ public class CoreCompetency {
         if (this == o) return true;
         if (!(o instanceof CoreCompetency)) return false;
         CoreCompetency that = (CoreCompetency) o;
-        return id == that.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
-    //  ONLY a getter for the 'id' field.
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
-    public int getId() {
-        return id;
+        return this.getId() == that.getId();
     }
 }
---
+++ src/org/launchcode/techjobs_oo/JobField.java
@@ -0,0 +1,41 @@
+package org.launchcode.techjobs_oo;
+
+import java.util.Objects;
+
+public abstract class JobField {
+    private String value;
+    private int id;
+    private static int nextId = 1;
+
+    public JobField() {
+        this.id = nextId;
+        nextId++;
+    }
+
+    public JobField(String newValue) {
+        this();
+        this.value = newValue;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
